503351f2774c09745e3acc5209d86697c313e844
AminMusah <ameenmusah77@gmail.com>
2023-01-03 21:27:30 -0600

    update vite.config.js

--- blog/vite.config.js
+++ blog/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy: {
       "/api":{
-        target: "http://localhost:8000/",
+        target: "https://server-nine-kappa.vercel.app/",
       },
     },
   },
